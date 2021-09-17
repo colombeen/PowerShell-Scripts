2a2b54f327935c64e979ef6f5e227e34558d8146
Colombeen Jarno <Jarno.Colombeen@district09.gent>
2021-09-17 09:17:29 +0200

    Create Optimize-String.ps1
    New function to clean up strings for easier processing

---
+++ Optimize-String.ps1
@@ -0,0 +1,104 @@
+﻿Function Optimize-String
+{
+  Param
+  (
+    [Parameter(
+        Mandatory = $true,
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $true,
+        Position = 0
+    )]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $String,
+    
+    [ValidateSet('Diacritics', 'AllSpaces', 'LeadingSpaces', 'TrailingSpaces', 'DoubleSpaces', 'LowerCase', 'UpperCase', 'Numbers', 'SpecialChars', 'NewLines')]
+    [string[]]
+    $Remove,
+    
+    [ValidateSet('LowerToUpperCase', 'UpperToLowerCase', 'DashToHyphen')]
+    [string[]]
+    $Replace
+  )
+
+  Begin
+  {
+  }
+  
+  Process
+  {
+    If ($Remove.Count -gt 0)
+    {
+      If ('Diacritics' -in $Remove)
+      {
+        $String = [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding('Cyrillic').GetBytes($String))
+      }
+      
+      If ('LowerCase' -in $Remove)
+      {
+        $String = $String -creplace '[\p{Ll}]'
+      }
+      
+      If ('UpperCase' -in $Remove)
+      {
+        $String = $String -creplace '[\p{Lu}]'
+      }
+      
+      If ('Numbers' -in $Remove)
+      {
+        $String = $String -creplace '[\p{N}]'
+      }
+      
+      If ('SpecialChars' -in $Remove)
+      {
+        $String = $String -creplace '[^\p{L}\p{Nd}\p{Z}\r\n]'
+      }
+      
+      If ('NewLines' -in $Remove)
+      {
+        $String = $String -creplace '[\r\n]'
+      }
+      
+      If ('AllSpaces' -in $Remove)
+      {
+        $String = $String -replace ' '
+      }
+      
+      If ('LeadingSpaces' -in $Remove)
+      {
+        $String = $String.TrimStart()
+      }
+      
+      If ('TrailingSpaces' -in $Remove)
+      {
+        $String = $String.TrimEnd()
+      }
+      
+      If ('DoubleSpaces' -in $Remove)
+      {
+        $String = $String -creplace '[\p{Zs}]+', ' '
+      }
+    }
+    
+    
+    If ($Replace.Count -gt 0)
+    {
+      If ('DashToHyphen' -in $Replace)
+      {
+        $String = $String -creplace '[\p{Pd}]', '-'
+      }
+      
+      If ('LowerToUpperCase' -in $Replace)
+      {
+        $String = $String.ToUpper()
+      }
+      
+      If ('UpperToLowerCase' -in $Replace)
+      {
+        $String = $String.ToLower()
+      }
+    }
+    
+    Write-Output $($String -as [string])
+  }
+}
